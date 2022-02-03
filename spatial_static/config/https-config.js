07a0269f4518d0279def28cc6d40d726dbfcb847
Braden Riggs <48164242+Briggs599@users.noreply.github.com>
2022-02-03 11:28:29 -0800

    Delete spatial_static directory

--- spatial_static/config/https-config.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-module.exports = {
-    cert: fs.readFileSync(__dirname + "/localhost.pem"),
-    key: fs.readFileSync(__dirname + "/localhost-key.pem"),
-    passphrase: "12345",
-};
